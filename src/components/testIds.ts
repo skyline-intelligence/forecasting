65156af70a549435e88e1f3c6595dd8e7bf4f90e
skyline-intelligence <alex@skyline-intelligence.com>
2025-05-02 10:24:03 +0800

    Add files via upload

---
+++ src/components/testIds.ts
@@ -0,0 +1,10 @@
+export const testIds = {
+  appConfig: {
+    apiKey: 'data-testid ac-api-key',
+    apiUrl: 'data-testid ac-api-url',
+    submit: 'data-testid ac-submit-form',
+  },
+  pageTwo: {
+    container: 'data-testid pg-two-container',
+  },
+};
